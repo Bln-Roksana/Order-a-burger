94b49cfb2a612f27ed667f6b7cd0748e7085b456
Roksana <roksana.bln@gmail.com>
2020-11-13 14:34:20 +0000

    Build control for ingredient added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,25 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    let transIngredients=Object.keys(props.ingredients) // this gives an array of keys
+        .map(e => {
+            return [...Array(props.ingredients[e])].map((_, i) => {
+                return <BurgerIngredient key={e+i} type={e} />
+            });
+        })
+        .reduce((prevVal, currVal) => {
+            return prevVal.concat(currVal)
+        },[])
+
+        console.log(transIngredients);
+    if (transIngredients.length===0){
+        transIngredients=<p>Please start addinng ingredients!</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='meat'/>
+            {transIngredients}
             <BurgerIngredient type='bread-bottom'/>
         </div>
     )
